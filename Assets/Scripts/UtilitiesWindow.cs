500c9c584a9654ba352fb1cf7ac7866dcb1d84c7
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-05-16 01:35:34 +0300

    sfx update

---
+++ Assets/Scripts/UtilitiesWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using DungeonScripts;
+using TMPro;
+using Random = System.Random;
+
+public class UtilitiesWindow : EditorWindow
+{
+    private GameObject _parent;
+    
+    [MenuItem("Supreme/Dungeon Generator")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+    
+    void OnGUI()
+    {
+        GUILayout.Label("Object", EditorStyles.largeLabel);
+        _parent = EditorGUILayout.ObjectField("Parent", _parent, typeof(GameObject)) as GameObject;
+
+        if (GUILayout.Button("Delete Inactive Children"))
+        {
+
+        }
+    }
+}
